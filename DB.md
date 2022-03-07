7188cc0f06502caa319059e1f942f9590d1fc839
Deepak Kumar <deepak.kumar07@nagarro.com>
2022-03-08 01:54:14 +0530

    7th March 2022 - Class

---
+++ DB.md
@@ -0,0 +1,48 @@
+Online Exam System
+
+users - table - list of students, techers list, admin
+
+
+------------------------------------------------
+
+Database
+MySQL - SQL - Structed Database - Data table form
+ -- Structured Query language
+MongoDB - ORM - Non-Structed Database - Object
+  -- Object Relation Model
+
+Schema - 
+Users = {
+    userid: type, limit
+    name: type, limit
+    email: type, limit
+    limit: type, limit
+}
+
+Compass - GUI for mongodb - database creaion, tables, data
+wamp/xamp - phpMyadmin - sql database
+
+MongoDB - 
+Local - https://www.mongodb.com/try/download/community (msi install)
+Online - https://www.mongodb.com/
+https://account.mongodb.com/account/login
+
+
+userid  name    email       password
+0001    deepak  deepak123   123
+0002    abc     abc123      123
+
+[
+    {
+        userid: 0001,
+        name: deepak,
+        email: deepak123,
+        password: 123
+    },
+    {
+        userid: 0002,
+        name: abc,
+        email: abc123,
+        password: 123
+    }
+]
---
+++ models/User.js
@@ -0,0 +1,58 @@
+// Mongoose - Library - ORM - ExpressJS
+
+const mongoose = require('mongoose');
+
+
+const UserSchema = mongoose.Schema({
+    userid: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String
+    },
+    pincode: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    dob: {
+        type: Date
+    },
+    created: {
+        type: Date,
+        required: true
+    }
+},{strict: true});
+
+
+module.exports = mongoose.model('User', UserSchema);
